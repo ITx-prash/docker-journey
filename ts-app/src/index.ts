c1b83ef6ebdf4e8bbba3c70cd29c6b5135e67a2b
Prashant Adhikari <prashantadhikari440@gmail.com>
2026-03-17 20:45:23 +0545

    feat: add postgres and redis connections

--- ts-app/src/index.ts
+++ ts-app/src/index.ts
@@ -1,8 +1,29 @@
 import http from "http";
+import Redis from "ioredis";
+import { Client } from "pg";
 import app from "./app/server";
 
-function init() {
+async function init() {
   try {
+    // Redis Connection
+    console.log(`Connecting Redis...`);
+    const redis = new Redis("redis://localhost:6379", { lazyConnect: true });
+    await redis.connect();
+    console.log(`Redis Connection Success...`);
+
+    // Postgresql Connection
+    console.log(`Connecting Postgres...`);
+    const client = new Client({
+      host: "localhost",
+      port: 5432,
+      database: "postgres",
+      user: "postgres",
+      password: "1234",
+    });
+    await client.connect();
+
+    console.log(`Postgres Connection Success...`);
+    //http server stuff
     const PORT = process.env.PORT ? +process.env.PORT : 8000;
     const server = http.createServer(app);
     server.listen(PORT, () =>
